27dd64eabd067addb48421e38c36adc02b7a6564
TorruTech <javiertp97@gmail.com>
2025-05-27 21:15:34 +0200

    api key updated

--- src/auth/firebaseConfig.js
+++ src/auth/firebaseConfig.js
@@ -2,7 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCjt1e9Jbe1rp3AXsAvSv2ynMdKzad3B_E",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
